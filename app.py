21662e04967e17380a0f3080a1c7cf8056fb0aee
KayvonPaygon <112442143+KayvonPaygon@users.noreply.github.com>
2023-11-29 22:53:53 -0600

    Added base files that will be needed

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/reservations')
+def reservations():
+    return render_template('reservations.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
---
+++ methods.py
@@ -0,0 +1,23 @@
+def calculate_total_sales():
+
+    return 100.00
+
+def generate_code(first_name):
+    class_name = "INFOTC4320"
+
+    # all it does is put the letters of the first_name between the letters of class_name
+    # just make an algorithm that does that and doesnt throw an out_of_bounds error
+
+    return "KIeNvFiOnTC4320"
+
+def get_cost_matrix():
+    cost_matrix = [[100, 75, 50, 100] for row in range(12)]
+    return cost_matrix
+
+def get_seats():
+
+    return ['O', 'X', 'O', 'X']
+
+def write_to_file(data_array, file_path):
+
+    return False
